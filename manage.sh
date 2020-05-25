2fb59f865bbbfa7a0115161066e1cd47aaaa326d
lucien <thewebress@gmail.com>
2020-05-25 09:45:37 +0800

    Update manage.sh
    修改排版缩进

--- manage.sh
+++ manage.sh
@@ -9,48 +9,48 @@ MASTERAPP=$APPNAME.Master
 cd $SERVER
 
 start(){
-	echo "start service..."
-	if [ ! -f $PIDPATH ]; then
-		startone
-	else
-		echo "pid existed"
-		stopone
-		startone
-	fi
-	echo "start service completed!"	    
+    echo "start service..."
+    if [ ! -f $PIDPATH ]; then
+      startone
+    else
+      echo "pid existed"
+      stopone
+      startone
+    fi
+    echo "start service completed!"	   
 }
 
 startone(){
-	nohup php bin/hyperf.php start > $SERVER/runtime/log.out 2>&1 &
-	#nohup php bin/hyperf.php start > /dev/null 2>&1 &
-	tail -f $SERVER/runtime/log.out
+    nohup php bin/hyperf.php start > $SERVER/runtime/log.out 2>&1 &
+    #nohup php bin/hyperf.php start > /dev/null 2>&1 &
+    tail -f $SERVER/runtime/log.out
 }
 
 stopone(){
-	echo "stop service..."
-	kill `cat $PIDPATH`
-	rm -rf $PIDPATH
-	sleep 2
-	echo "stop service $APPNAME completed!"
+    echo "stop service..."
+    kill `cat $PIDPATH`
+    rm -rf $PIDPATH
+    sleep 2
+    echo "stop service $APPNAME completed!"
 }
 
 stop(){
-  echo "stop $MASTERAPP service..."
-  ps -ef | grep $MASTERAPP | grep -v grep | awk '{print $2}' | xargs kill -9 2>&1
-  rm -rf $PIDPATH
-  sleep 2
-	echo "stop service $APPNAME completed!"
+    echo "stop $MASTERAPP service..."
+    ps -ef | grep $MASTERAPP | grep -v grep | awk '{print $2}' | xargs kill -9 2>&1
+    rm -rf $PIDPATH
+    sleep 2
+    echo "stop service $APPNAME completed!"
 }
 
 startdev(){
-  #监听文件变化自动重启Hyperf
-  #依赖https://github.com/ha-ni-cc/hyperf-watch 下载单文件watch到根目录 根据情况修改扫描间隔
-	if [ ! -f watch ]; then
-		echo -e  "依赖https://github.com/ha-ni-cc/hyperf-watch 下载单文件watch到根目录"
-		exit 1
-	fi
-  	echo "start $MASTERAPP service dev..."
-	nohup php watch -c > $SERVER/runtime/log.out 2>&1 &
+    #监听文件变化自动重启Hyperf
+    #依赖https://github.com/ha-ni-cc/hyperf-watch 下载单文件watch到根目录 根据情况修改扫描间隔
+    if [ ! -f watch ]; then
+      echo -e  "依赖https://github.com/ha-ni-cc/hyperf-watch 下载单文件watch到根目录"
+      exit 1
+    fi
+    echo "start $MASTERAPP service dev..."
+    nohup php watch > $SERVER/runtime/log.out 2>&1 &
 }
 
 restart()
